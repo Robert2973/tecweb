04625775ee460cb3114bb274e6c223766e276248
Robert <roberto.hernandezap@alumno.buap.mx>
2024-09-04 19:56:24 -0600

    Creacion del archivo

---
+++ practicas/p05/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="es">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Práctica PHP</title>
+</head>
+<body>
+    <h1>Práctica PHP</h1>
+</body>
+</html>
+
