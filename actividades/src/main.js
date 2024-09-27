aeb90e9acbcc98bee39bb06cd0fca9328d266623
Robert <roberto.hernandezap@alumno.buap.mx>
2024-09-27 13:44:49 -0600

    Ejemplo 2

--- actividades/src/main.js
+++ actividades/src/main.js
@@ -1,14 +1,13 @@
 //FUNICOND E EJEMPLO
-function getDatos()
-{
+function getDatos() {
     var nombre = window.prompt("Nombre: ", "");
     var edad = prompt("Edad: ", "");
 
     var div1 = document.getElementById('nombre');
-    div1.innerHTML= '<h3> Nombre '+ nombre + '</h3>';
+    div1.innerHTML = '<h3> Nombre ' + nombre + '</h3>';
 
     var div2 = document.getElementById('edad');
-    div2.innerHTML='<h3> Edad: '+ edad + '</h3>';
+    div2.innerHTML = '<h3> Edad: ' + edad + '</h3>';
 }
 
 
@@ -19,3 +18,18 @@ function impresion() {
     document.getElementById('resultados').appendChild(Hola);
 }
 
+//Ejemplo 2
+function variables() {
+    var nombre = 'Juan';
+        var edad = 10;
+        var altura = 1.92;
+        var casado = false;
+
+        // Insertar las respuestas en los elementos div
+        document.getElementById('ejercicio1').innerHTML += nombre;
+        document.getElementById('ejercicio2').innerHTML += edad;
+        document.getElementById('ejercicio3').innerHTML += altura;
+        document.getElementById('ejercicio4').innerHTML += casado;
+}
+
+//Ejemplo 2
